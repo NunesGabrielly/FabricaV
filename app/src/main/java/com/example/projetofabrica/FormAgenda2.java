3951b3788266cba75448c7e8cdadd946b1a068c6
gabrielly.rodrigues <190053@facens.br>
2023-05-11 16:54:51 -0300

    four commit

--- app/src/main/java/com/example/projetofabrica/FormAgenda2.java
+++ app/src/main/java/com/example/projetofabrica/FormAgenda2.java
@@ -13,24 +13,11 @@ import java.util.Objects;
 
 public class FormAgenda2 extends AppCompatActivity {
 
-    Button bt_sair;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_agenda2);
         getSupportActionBar().hide();
 
-        bt_sair = (Button) findViewById(R.id.bt_salvar);
-
-        bt_sair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(FormAgenda2.this, FormLogin.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
